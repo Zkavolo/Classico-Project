31453e3903aeefce9bd4c18b54309f3b27c22bff
Fernando <fernandosianturi58@gmail.com>
2022-06-14 11:04:55 +0700

    configuration routes and controllers, adding landing page, adding boostrap 3, adding style.css, deleting welcome_message.php

---
+++ application/controllers/Main.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Main extends CI_Controller
+{
+
+	public function index()
+	{
+		$this->load->view('home/landing_page');
+	}
+}
