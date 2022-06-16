124f9198c60fa682c1b7c921f156ea91cd98fd1b
Fernando <fernandosianturi58@gmail.com>
2022-06-16 10:10:16 +0700

    editing controllers Main.php

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -4,13 +4,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Main extends CI_Controller
 {
 
-	// public function index()
-	// {
-	// 	$this->load->view('home/landing_page.php');
-	// }
-
 	public function index()
 	{
-		$this->load->view('login.php');
+		$this->load->view('home/landing_page.php');
 	}
 }
